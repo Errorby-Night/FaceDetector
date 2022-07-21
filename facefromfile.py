a05c413d6172b4a416628e1cbd9aa26a72f9f6bd
Srijan Dutta <60324903+Errorby-Night@users.noreply.github.com>
2022-07-21 11:16:17 +0530

    Create facefromfile.py

---
+++ facefromfile.py
@@ -0,0 +1,16 @@
+import cv2
+
+face = cv2.CascadeClassifier("frontalface.xml")
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, img = cap.read()
+    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face.detectMultiScale(grey, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow('Faces', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
