12e60fab1578484ff6f4ba6f510d6eef0692393b
Srijan Dutta <60324903+Errorby-Night@users.noreply.github.com>
2022-07-25 09:57:58 +0530

    Update facefromcam.py
    Run this file to detect face directly from your webcam

--- facefromcam.py
+++ facefromcam.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0)
 while True:
     _, img = cap.read()
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face.detectMultiScale(grey, 1.1, 4)
+    faces = face.detectMultiScale(grey, 1.2, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
     cv2.imshow('Faces', img)
